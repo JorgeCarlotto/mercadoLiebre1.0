7d4cd05facd4369322486bdb090b04a778355f4d
JorgeCarlotto <jorgemcarlotto@gmail.com>
2021-09-09 22:12:49 -0300

    Aplicacion de formularios

--- app.js
+++ app.js
@@ -10,7 +10,15 @@ app.get('/',(req, res)=>{
     res.sendFile(path.join(__dirname,'/views/home.html'))
 })
 
+app.get('/register',(req, res)=>{
+    res.sendFile(path.join(__dirname,'/views/register.html'))
+})
+
+app.get('/login',(req, res)=>{
+    res.sendFile(path.join(__dirname,'/views/login.html'))
+})
 
 app.listen(3000,()=>{
     console.log('Servidor corriendo en http://localhost:3000')
 })
+
